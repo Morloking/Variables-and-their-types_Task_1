2830bf9077724def83c2851ca52bbfcf3e67df76
Morloking <dyubenkosergei@yandex.ru>
2026-03-04 23:42:50 +0300

    Add files via upload

---
+++ Sector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    cout << "Введите число:\n";
+    int x{ 0 };
+    cin >> x;
+    cout << "Вы ввели:\n";
+    cout << x;
+}
